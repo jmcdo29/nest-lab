df10f7d20daa623e37bd0b1d21187d01063c2d5c
Jay McDoniel <jmcdo29@gmail.com>
2022-10-15 10:41:35 -0700

    fix: update the register and register async

--- packages/fastify-multer/src/lib/fastify-multer.module.ts
+++ packages/fastify-multer/src/lib/fastify-multer.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
 import { FastifyCoreModule } from './fastify-multer-core.module';
-import { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } from './fastify-multer.module-definition';
+import {
+  ASYNC_OPTIONS_TYPE,
+  ConfigurableModuleClass,
+  MODULE_OPTIONS_TOKEN,
+  OPTIONS_TYPE,
+} from './fastify-multer.module-definition';
 
 @Module({
   imports: [FastifyCoreModule],
   controllers: [],
   providers: [],
-  exports: [MODULE_OPTIONS_TOKEN],
+  exports: [],
 })
-export class FastifyMulterModule extends ConfigurableModuleClass {}
+export class FastifyMulterModule extends ConfigurableModuleClass {
+  static register(options: typeof OPTIONS_TYPE) {
+    return {
+      ...super.register(options),
+      exports: [MODULE_OPTIONS_TOKEN],
+    };
+  }
+
+  static registerAsync(options: typeof ASYNC_OPTIONS_TYPE) {
+    return {
+      ...super.registerAsync(options),
+      exports: [MODULE_OPTIONS_TOKEN],
+    };
+  }
+}
