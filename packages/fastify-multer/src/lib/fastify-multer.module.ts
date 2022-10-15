52e91e663969dcbf1a5cedb419b31da84abd158d
Jay McDoniel <jmcdo29@gmail.com>
2022-10-15 10:25:28 -0700

    fix: allow options to be set in the module
    By adding the token to the `exports` array of the `FastifyMulterModule`
    devs are now able to set the options in the module and let them persist
    via the interceptor without having to worry about re-setting them
    later.
    
    fix: #14

--- packages/fastify-multer/src/lib/fastify-multer.module.ts
+++ packages/fastify-multer/src/lib/fastify-multer.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FastifyCoreModule } from './fastify-multer-core.module';
-import { ConfigurableModuleClass } from './fastify-multer.module-definition';
+import { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } from './fastify-multer.module-definition';
 
 @Module({
   imports: [FastifyCoreModule],
   controllers: [],
   providers: [],
-  exports: [],
+  exports: [MODULE_OPTIONS_TOKEN],
 })
 export class FastifyMulterModule extends ConfigurableModuleClass {}
